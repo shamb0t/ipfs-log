455927a135bda8ea4215b56f55e263d800920dbd
shamb0t <shams.methnani@gmail.com>
2018-07-31 21:30:14 +0200

    acl tests

--- test/acl.test.js
+++ test/acl.test.js
@@ -6,29 +6,19 @@ describe('ACL', function() {
   let acl
   beforeEach (() => {
     acl = new ACL(getTestACL())
-    // acl.subscribeEvents(acl._capabilities)
   })
 
   it('creates an acl', () => {
-    // IPFS PeerID
-    // Ethereum wallet address
-    // etc.
-    assert.deepEqual(acl._capabilities, { write: ['0xaC39b311DCEb2A4b2f5d8461c1cdaF756F4F7Ae9']})
+    assert.deepEqual(acl._capabilities, {})
   })
 
   it('adds capability', () => {
-    // IPFS PeerID
-    // Ethereum wallet address
-    // etc.
     acl.add(acl._capabilities, 'write', 'key', 'ethAddr')
     var permitted = acl.canAppend('key')
     assert.equal(permitted, true)
   })
 
   it('key not in acl cannot append', () => {
-    // IPFS PeerID
-    // Ethereum wallet address
-    // etc.
     acl.add(acl._capabilities, 'write', 'key', 'ethAddr')
     var permitted = acl.canAppend('badkey')
     assert.equal(permitted, false)
--- test/utils/test-entry-acl.js
+++ test/utils/test-entry-acl.js
@@ -1,7 +1,5 @@
 const acl = require('../../src/acl')
-const DEFAULT_PUBLIC_KEY = 'key'
-const DEFAULT_SIGNATURE = 'deadbeef'
-let DEFAULT_CAPS = { write: ['0xaC39b311DCEb2A4b2f5d8461c1cdaF756F4F7Ae9']} //0xaC39b311DCEb2A4b2f5d8461c1cdaF756F4F7Ae9
+let DEFAULT_CAPS = {}
 const load = (capabilities) => DEFAULT_CAPS = capabilities
 const subscribeEvents = (capabilities) =>  Object.assign(capabilities, DEFAULT_CAPS)
 const add = (capabilities, capability, toKey, ethAddress) => capabilities[capability] ? capabilities[capability].push(toKey) : capabilities[capability] = [toKey]
@@ -11,5 +9,3 @@ const getTestACL = () => {
 }
 
 module.exports = getTestACL
-module.exports.DEFAULT_SIGNATURE = DEFAULT_SIGNATURE
-module.exports.DEFAULT_PUBLIC_KEY = DEFAULT_PUBLIC_KEY
