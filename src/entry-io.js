85c8e071e2ba511830c44285ff0f86676f012ee9
haad <haad@haja.io>
2018-08-28 13:55:21 +0200

    Make exclude array check in entry-io more explicit

--- src/entry-io.js
+++ src/entry-io.js
@@ -37,8 +37,9 @@ class EntryIO {
     const addToLoadingQueue = e => loadingQueue.push(e)
 
     // Add entries that we don't need to fetch to the "cache"
+    exclude = exclude && Array.isArray(exclude) ? exclude : []
     var addToExcludeCache = e => cache[e.hash] = e
-    exclude && exclude.length && exclude.forEach(addToExcludeCache)
+    exclude.forEach(addToExcludeCache)
 
     const shouldFetchMore = () => {
       return loadingQueue.length > 0
